2c2249bcfac23939a71ce9e13953142283441685
Michael Johnson <Slicepotato@users.noreply.github.com>
2018-01-01 20:31:27 -0500

    Delete config.js

--- bird-catcher/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: '9YKTN7jo8bHYXTGY2ikFXn6Sy',
-  consumer_secret: 'EmFNfljxnTEJMAqYnuroPAIVxPPNdmB79ySBuGWudPgryjIbwZ',
-  access_token: '272671259-w4Ek2jxLbcqvucUIFJMSRs8YxoeckMgQ34kTgeh1',
-  access_token_secret: 'iteT6bs8JeItqu1B2QyHWPkA2uuVvFcpJlvmaLu6RnJYv'
-}
