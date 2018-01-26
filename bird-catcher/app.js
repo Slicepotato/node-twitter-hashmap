e4df75769116e914528fe8b0bc20edecdc189387
slicepotato <m.ianjohnson@gmail.com>
2018-01-25 22:54:30 -0500

    production updates

--- bird-catcher/app.js
+++ bird-catcher/app.js
@@ -3,7 +3,11 @@ var config = require('./config');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io').listen(http);
+
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 app.use(express.static(__dirname + '/public'));
 app.use('/img',express.static(__dirname + '/public/img'));
@@ -20,16 +24,16 @@ io.on('connection', function(socket){
     socket.emit("hashlist", { hash: hashtags });
     var stream = twit.stream('statuses/filter', { track: hashtags, language: 'en' }); // get is the function to search the tweet which three paramaters 'search/tweets',params and a callback function.
     stream.on('tweet', function (tweet) {
-       if(tweet.coordinates != null) { 
+       if(tweet.coordinates != null) {
             var outputPoint = {"user": tweet.user.screen_name, "time": tweet.created_at, "text": tweet.text, "lat": tweet.coordinates.coordinates[0],"lng": tweet.coordinates.coordinates[1]};
             socket.emit("twitter-stream", outputPoint);
-            
+
             console.log(outputPoint);
         }
         /* console.log(tweet); */
     });
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
